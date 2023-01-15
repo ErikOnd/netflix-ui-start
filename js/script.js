c77e9dc6d954f6f0eb5a72ac7e6d5dc53f0a8adb
Erik <erik.ondra@yahoo.com>
2023-01-15 17:52:12 +0100

    hover effect for profile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $('.carousel').carousel({
     interval: false,
 });
+
+
+let profilePicture = document.querySelectorAll('.d-lg-block')[3];
+let accountInfo = document.getElementsByClassName('account-info')[0];
+
+profilePicture.addEventListener('mouseover', function () {
+    accountInfo.style.opacity = "1";
+})
+
+profilePicture.addEventListener('mouseleave', function () {
+    accountInfo.style.opacity = "0";
+})
